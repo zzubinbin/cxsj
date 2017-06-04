580801e7ff66035f9ff42c6c4fd48ca22a3b537c
wanglikang <1024196018@qq.com>
2017-06-04 23:07:20 +0800

    添加某些DB映射类

--- src/com/zzu/cxsj/dbBean/DBOrder.java
+++ src/com/zzu/cxsj/dbBean/DBOrder.java
@@ -1,5 +1,13 @@
 package com.zzu.cxsj.dbBean;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import com.alibaba.fastjson.JSONObject;
+
+@Entity
+@Table(name = "order")
 public class DBOrder {
 
 }
