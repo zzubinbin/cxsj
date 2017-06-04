c83d3074022c9c18480979acb80877bbdc9bb5c1
wanglikang <1024196018@qq.com>
2017-06-04 23:07:49 +0800

    添加少量DB映射类

---
+++ src/com/zzu/cxsj/dbBean/DBXuanke.java
@@ -0,0 +1,14 @@
+package com.zzu.cxsj.dbBean;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.alibaba.fastjson.JSONObject;
+
+@Entity
+@Table(name = "xuanshu")
+public class DBXuanke {
+
+}
